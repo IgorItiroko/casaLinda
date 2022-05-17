b31cc38f3d6e0c53816e382eadf332b64c22ac5c
Igor Mac <igorshinji@yahoo.com>
2022-05-17 17:12:37 -0300

    funcionalidades estoque feitas

--- estoque.py
+++ estoque.py
@@ -1,4 +1,5 @@
 import json
+from tokenize import String
 class Estoque:
     def incluirProduto(self, nome, desc, preco, qtde):
         with open("estoque.json") as file:
@@ -7,7 +8,7 @@ class Estoque:
         data["Produtos"].append(list)
         with open("estoque.json","w") as file2:
             json.dump(data, file2, indent=4)
-            
+
     def excluirProduto(self, codProd):
         with open("estoque.json") as file:
             data = json.load(file)
@@ -18,9 +19,61 @@ class Estoque:
         with open("estoque.json","w") as file2:
             json.dump(data, file2, indent=4)
     
+    def pesquisarProdutoPorNome(self, nome):
+        with open("estoque.json") as file:
+            data = json.load(file)
+        list = []
+        for index in range(0, len(data["Produtos"])):
+            if data["Produtos"][index]["Nome"] == nome:
+                list.append(data["Produtos"][index])
+        return list
+        
+    def pesquisarPorPrecoMax(self, precMax):
+        with open("estoque.json") as file:
+            data = json.load(file)
+        list = []
+        for index in range(0, len(data["Produtos"])):
+            if data["Produtos"][index]["Preco"] < precMax:
+                list.append(data["Produtos"][index])
+        return list
 
-bob = Estoque()
-bob.excluirProduto(9)
+    def pesquisarPorPrecoMin(self, precMin):
+        with open("estoque.json") as file:
+            data = json.load(file)
+        list = []
+        for index in range(0, len(data["Produtos"])):
+            if data["Produtos"][index]["Preco"] > precMin:
+                list.append(data["Produtos"][index])
+        return list
+
+    def faltaDeEstoque(self):
+        with open("estoque.json") as file:
+            data = json.load(file)
+        list = []
+        for index in range(0, len(data["Produtos"])):
+            if data["Produtos"][index]["Qtde"] == 0:
+                list.append(data["Produtos"][index])
+        return list
+
+    def estoqueBaixo(self):
+        with open("estoque.json") as file:
+            data = json.load(file)
+        list = []
+        for index in range(0, len(data["Produtos"])):
+            if data["Produtos"][index]["Qtde"] < 2:
+                list.append(data["Produtos"][index])
+        return list
+
+    def editarProduto(self, codProd, novoNome, novaDesc, novoPreco):
+        with open("estoque.json") as file:
+            data = json.load(file)
+        for index in range(0, len(data["Produtos"])):
+            if data["Produtos"][index]["Cod"] == codProd:
+                data["Produtos"][index]["Nome"] = novoNome
+                data["Produtos"][index]["Desc"] = novaDesc
+                data["Produtos"][index]["Preco"] = novoPreco
+        with open("estoque.json","w") as file2:
+            json.dump(data, file2, indent=4)
 
 
 
