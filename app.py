62c68335fd32697b11a4a1e6b0f00b0df5c28c42
Igor Mac <igorshinji@yahoo.com>
2022-05-17 15:23:33 -0300

    Manipulacao estoque funcionando

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ class Login:
         for i in listaLogin:
             if usuario == format(i['usuario']) and senha == format(i['senha']):
                 return True
-        return False
-    print(verificarLogin('teste1','teste2')) #return false
-    print(verificarLogin('lucia','casalinda')) #return true
+        return False 
+
+
+
