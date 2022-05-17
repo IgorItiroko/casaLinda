b36850f7f2dcffe14095dd697f3d4113f0834bbb
Igor Mac <igorshinji@yahoo.com>
2022-05-17 16:39:08 -0300

    estoque delete

--- estoque.py
+++ estoque.py
@@ -1,15 +1,26 @@
 import json
 class Estoque:
-
     def incluirProduto(self, nome, desc, preco, qtde):
         with open("estoque.json") as file:
             data = json.load(file)
-        list = [{"Nome": nome, "Desc": desc, "Preco": preco, "Cod": len(data) + 1,"Qtde": qtde}]
-        data.append(list)
+        list = {"Nome": nome, "Desc": desc, "Preco": preco, "Cod": len(data["Produtos"]) + 1,"Qtde": qtde}
+        data["Produtos"].append(list)
         with open("estoque.json","w") as file2:
             json.dump(data, file2, indent=4)
+            
+    def excluirProduto(self, codProd):
+        with open("estoque.json") as file:
+            data = json.load(file)
+        for index in range(0, len(data["Produtos"])):
+            if data["Produtos"][index]["Cod"] == codProd:
+                data["Produtos"].pop(index)
+                break 
+        with open("estoque.json","w") as file2:
+            json.dump(data, file2, indent=4)
+    
+
 bob = Estoque()
-bob.incluirProduto("Alho", "para comer", 10.0, 5)
+bob.excluirProduto(9)
 
 
 
