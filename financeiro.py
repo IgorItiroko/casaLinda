7592999710c91c46de9049ca7627d2b0313cd427
Igor Mac <igorshinji@yahoo.com>
2022-05-19 15:34:52 -0300

    variavel encontrado, e incluir estoque como boolean

--- financeiro.py
+++ financeiro.py
@@ -1,6 +1,7 @@
 import json
 class Financeiro:
     def adicionarFinanceiro(self, cod_Produto, tipo, qtde, valor):
+        encontrado = 0
         with open("financeiro.json") as fileFinanceiro:
             dataFinanceiro = json.load(fileFinanceiro)
         with open("estoque.json") as fileEstoque:
@@ -9,14 +10,19 @@ class Financeiro:
         dataFinanceiro["Registros"].append(list)
         for index in range(0, len(dataEstoque["Produtos"])):
             if(dataEstoque["Produtos"][index]["Cod"] == cod_Produto):
-                    if(tipo == "C"):
-                        dataEstoque["Produtos"][index]["Qtde"] += qtde
-                    else:
-                        dataEstoque["Produtos"][index]["Qtde"] -= qtde
-        with open("estoque.json","w") as file3:
-            json.dump(dataEstoque, file3, indent=4)
-        with open("financeiro.json","w") as file2:
-            json.dump(dataFinanceiro, file2, indent=4)
+                encontrado = 1
+                if(tipo == "C"):
+                    dataEstoque["Produtos"][index]["Qtde"] += qtde
+                else:
+                    dataEstoque["Produtos"][index]["Qtde"] -= qtde
+        if(encontrado == 1):
+            with open("estoque.json","w") as file3:
+                json.dump(dataEstoque, file3, indent=4)
+            with open("financeiro.json","w") as file2:
+                json.dump(dataFinanceiro, file2, indent=4)
+            return True
+        else: 
+            return False
     
 
 bob = Financeiro()
