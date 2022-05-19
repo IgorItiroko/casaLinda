35fd411d849c60b0567c007c58256d1c91770cb1
Igor Mac <igorshinji@yahoo.com>
2022-05-19 15:29:55 -0300

    trigger estoque feito

--- financeiro.py
+++ financeiro.py
@@ -1,12 +1,22 @@
 import json
 class Financeiro:
     def adicionarFinanceiro(self, cod_Produto, tipo, qtde, valor):
-        with open("financeiro.json") as file:
-            data = json.load(file)
-        list = {"Cod Compra": len(data["Registros"]) + 1, "Cod Produto": cod_Produto, "CompraVenda": tipo,"Qtde": qtde, "Valor Uni": valor}
-        data["Registros"].append(list)
+        with open("financeiro.json") as fileFinanceiro:
+            dataFinanceiro = json.load(fileFinanceiro)
+        with open("estoque.json") as fileEstoque:
+            dataEstoque = json.load(fileEstoque)
+        list = {"Cod Compra": len(dataFinanceiro["Registros"]) + 1, "Cod Produto": cod_Produto, "CompraVenda": tipo,"Qtde": qtde, "Valor Uni": valor}
+        dataFinanceiro["Registros"].append(list)
+        for index in range(0, len(dataEstoque["Produtos"])):
+            if(dataEstoque["Produtos"][index]["Cod"] == cod_Produto):
+                    if(tipo == "C"):
+                        dataEstoque["Produtos"][index]["Qtde"] += qtde
+                    else:
+                        dataEstoque["Produtos"][index]["Qtde"] -= qtde
+        with open("estoque.json","w") as file3:
+            json.dump(dataEstoque, file3, indent=4)
         with open("financeiro.json","w") as file2:
-            json.dump(data, file2, indent=4)
+            json.dump(dataFinanceiro, file2, indent=4)
     
 
 bob = Financeiro()
