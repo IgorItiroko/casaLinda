21dd73733b9f730c414079dc3d81900a3e1032be
mjmarcura <67600909+mjmarcura@users.noreply.github.com>
2022-05-19 15:58:57 -0300

    Erro login

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen, ScreenManager
 import json
-class Login:
-    def verificarLogin(usuario, senha):
+
+class LoginScreen(Screen):
+    pass
+
+class EstoqueScreen(Screen):
+    pass
+
+class windowManager(ScreenManager):
+    pass
+
+sm = windowManager()
+sm.add_widget(LoginScreen(name='login'))
+sm.add_widget(EstoqueScreen(name='estoque'))
+
+class MainApp(MDApp):
+    def build(self):
+        sm = Builder.load_file('Screen.kv')
+
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return sm
+
+    def verificarLogin(self, user, password):
         listaLogin = json.load(open('loginData.json', 'r'))
         for i in listaLogin:
-            if usuario == format(i['usuario']) and senha == format(i['senha']):
+            if user == format(i['usuario']) and password == format(i['senha']):
+                self.root.current = 'estoque'
                 return True
-        return False
-    print(verificarLogin('teste1','teste2')) #return false
-    print(verificarLogin('lucia','casalinda')) #return true
+            else:
+                self.root.ids.error_label.text = f'Usuário e/ou senha incorretos'
+                return False
+        
+
+MainApp().run()
